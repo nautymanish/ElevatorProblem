4db13b35f55cfc5fb20bc77e4a4112a6d0f3244e
nautymanish <manu3k2000@gmail.com>
2020-11-27 16:07:40 +0530

    Adding duplicacy

---
+++ test.cs
@@ -0,0 +1,13 @@
+namespace ElevatorProblem
+{
+    public class Person 
+    {
+        public int CurrentFloor { get; set; }
+        public Direction DirectionPressed { get; set; }
+        
+        public void Update(Elevator elevator)
+        {
+
+        }
+    }
+}
