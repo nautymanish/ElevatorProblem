b9b647329a44652d3cf40ae02376f8d852ce4d59
nautymanish <manu3k2000@gmail.com>
2020-11-27 16:34:27 +0530

    Addign issues

--- test.cs
+++ test.cs
@@ -4,10 +4,36 @@ namespace ElevatorProblem
     {
         public int CurrentFloor { get; set; }
         public Direction DirectionPressed { get; set; }
-        
+        private void DoFirst()
+        {
+        }
         public void Update(Elevator elevator)
         {
-
+            int num = 0;
+            // Non compliant code  
+                try  
+                {  
+                  // Code that might have exception  
+                    if (num < 0)  
+{  
+  DoFirst();  
+  
+}  
+else if (num > 1 && num < 3)  
+{  
+  DoFirst();  // Duplicate code  
+ 
+}  
+else  
+{  
+  DoSomething();  
+}  
+                }  
+                finally  
+                {  
+                   throw new Exception("Error in finally");  
+                }  
         }
+        
     }
 }
